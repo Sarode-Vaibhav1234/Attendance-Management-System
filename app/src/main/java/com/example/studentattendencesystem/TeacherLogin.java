f95ccf1915d332fb533c27b995e11cdea806d74b
Sarode-Vaibhav1234 <sarodevaibhav20@gmail.com>
2025-01-25 23:20:07 +0530

    TeacherSignup page added

--- app/src/main/java/com/example/studentattendencesystem/TeacherLogin.java
+++ app/src/main/java/com/example/studentattendencesystem/TeacherLogin.java
@@ -1,6 +1,9 @@
 package com.example.studentattendencesystem;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +11,13 @@ public class TeacherLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.teacher_login);
-
+        TextView tv1=findViewById(R.id.t1);
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i1=new Intent(getApplicationContext(), TeacherSignup.class);
+                startActivity(i1);
+            }
+        });
     }
 }
