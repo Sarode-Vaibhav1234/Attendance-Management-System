7f2810b8d9a0d2ff3ed6d751a6cf3b134114607a
Abhay5474 <125486846+Abhay5474@users.noreply.github.com>
2025-04-17 00:52:26 +0530

    ok

--- php-files/get_lectures.php
+++ php-files/get_lectures.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "attendanceDB"; // Replace with your database name
+$dbname = "attendancedb"; // Replace with your database name
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
